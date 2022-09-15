9d7b8de760e15d1d53fc8109c844fed3bc6a5f32
Laurent Vleminckx <laurentvleminckx@gmail.com>
2022-09-15 23:32:20 +0200

    Add moving slider

--- src/JamStemPlayer.js
+++ src/JamStemPlayer.js
@@ -1,23 +1,24 @@
 import ReactPlayer from 'react-player/lazy'
 import { createRef, useCallback, useRef, useState } from 'react'
-import { Button, Slider, Typography } from 'antd'
+import { Button, Typography } from 'antd'
 import { PauseCircleOutlined, PlayCircleOutlined, StepBackwardOutlined } from '@ant-design/icons';
 import 'antd/dist/antd.css'
 import PlayheadSlider from './PlayheadSlider';
+
+const twoDecimal = f => {
+    return Math.round((f + Number.EPSILON) * 100) / 100
+}
 
 export default function JamStemPlayer({ songDef }) {
     console.log("(rerender)");
 
     const [medias] = useState(() => songDef.tracks.map(t => songDef.medias[t][0])) // we only take first media by track for now
-    console.dir(medias)
     const duration = Math.max(...medias.map(m => m.duration))
-    console.log(duration);
     const readyMedias = useRef([])
     const [isAllMediaReady, setAllMediaReady] = useState(false)
     
     const [isPlaying, setIsPlaying] = useState(false)
-    const position = useRef(0.1)
-    const isSeeking = useRef(false)
+    const [lastPosition, setLastPosition] = useState(null)
     
     const shouldPlayWhenAllReady = useRef(false)
     const shouldSeekWhenAllReady = useRef(0)
@@ -26,28 +27,22 @@ export default function JamStemPlayer({ songDef }) {
     
     // when all medias are loaded, they are synchronised before playing
     function onReady(media) {
-        console.log(`   (a media is ready)`)
         readyMedias.current = [...readyMedias.current, medias.indexOf(media)]
         if(readyMedias.current.length === medias.length) {
-            console.log(`   (all medias ready !)`)
             setAllMediaReady(true)
             if(shouldSeekWhenAllReady.current !== null) {
-                console.log(`   (so let's seek)`)
                 seekTo(shouldSeekWhenAllReady.current)
                 shouldSeekWhenAllReady.current = null
             }
             if(shouldPlayWhenAllReady.current) {
-                console.log(`   (so lets play)`)
                 setIsPlaying(true)
                 shouldPlayWhenAllReady.current = false
             }
         }
     }
 
-    const seekTo = useCallback(newPosition => {
-        console.log(`       seekTo(${newPosition}) (current pos = ${position.current})`)
-        if(newPosition == position.current) {
-            console.log("YES YE SYES")
+    const seekTo = useCallback((newPosition) => {
+        if(!isPlaying && newPosition === lastPosition) {
             return
         }
 
@@ -57,22 +52,19 @@ export default function JamStemPlayer({ songDef }) {
         readyMedias.current = []
         setAllMediaReady(false)
 
-        position.current = newPosition.toFixed(2)
+        setLastPosition(twoDecimal(newPosition))
 
         playerRefs.current.forEach((ref, i) => ref.current.seekTo(newPosition + (medias[i].offset ?? 0)))
-        shouldPlayWhenAllReady.current = wasPlaying
-    }, [isPlaying, medias])
+        shouldPlayWhenAllReady.current = shouldPlayWhenAllReady.current || wasPlaying
+    }, [isPlaying, lastPosition, medias])
 
     // keep other players in sync when a player is seeked with the controls
-    /*function syncPlayersWith(playerIdx, newPosition) {
-        const globalPosition = (newPosition - medias[playerIdx].offset).toFixed(2)
-        if(position !== globalPosition) {
-            console.log(`[syncPlayersWith(${playerIdx},${newPosition})] at global pos ${globalPosition}`)
+    function syncPlayersWith(playerIdx, newPosition) {
+        const globalPosition = twoDecimal(newPosition - medias[playerIdx].offset)
+        if(lastPosition !== globalPosition) {
             seekTo(Math.max(globalPosition, 0))
-        } else {
-            console.log(`[syncPlayersWith] (unnecessary)`)
         }
-    }*/
+    }
 
     return (
         <div>
@@ -91,29 +83,28 @@ export default function JamStemPlayer({ songDef }) {
                 Seek middle
             </Button>
             
-            <PlayheadSlider min={0} max={duration} value={position.current}
-                    onMouseDown={() => isSeeking.current = true}
-                    onChange={() => {}}
-                    onMouseUp={(e) => {
-                        isSeeking.current = false
-                        seekTo(parseFloat(e.target.value))
-                    }}
+            <PlayheadSlider
+                duration={duration}
+                isPlaying={isPlaying}
+                lastPosition={lastPosition ?? 0}
+                step={0.2}
+                onSeek={p => seekTo(p)}
             />
             
             <div style={{display:'inline-grid', gridTemplateColumns: 'auto auto', gridTemplateRows: 'auto auto'}}>
             {medias.map((media, i) => <ReactPlayer
                                         key={media.name}
                                         url={songDef.basePath + '/' + media.url}
-                                        controls={false}
+                                        controls={true}
                                         volume={media.volume ?? 1}
                                         onReady={() => onReady(media)}
                                         playing={isPlaying}
                                         onPause={() => setIsPlaying(false)}
                                         onPlay={() => setIsPlaying(true)}
-                                        
+                                        onSeek={seconds => syncPlayersWith(i, seconds)}
                                         ref={playerRefs.current[i]} />)}
             </div>
 
         </div>
-  ); // onSeek={seconds => syncPlayersWith(i, seconds)} // (position : {position})
+  );
 }
--- src/PlayheadSlider.js
+++ src/PlayheadSlider.js
@@ -1,8 +1,28 @@
 import 'antd/dist/antd.css'
 import { Slider } from 'antd'
+import { useEffect, useState } from 'react'
 
-export default function PlayheadSlider({duration, position, isSeeking, seekTo, ...props}) {
+export default function PlayheadSlider({duration, step, isPlaying, lastPosition, onSeek, ...props}) {
 
-    return <Slider {...props}
-            />
+    const [position, setPosition] = useState(0)
+
+    useEffect(() => {
+        setPosition(lastPosition)
+    }, [lastPosition])
+
+    useEffect(() => {
+        if(isPlaying) {
+            const interval = setInterval(() => {
+                setPosition(p => p + step)
+            }, step*1000)
+            return () => clearInterval(interval)
+        }
+    }, [isPlaying, step])
+
+    return <Slider
+            min={0} max={duration} step={step}
+            value={position}
+            onChange={e => onSeek(e)}
+            tooltip={{formatter: s => Math.floor(s/60) + 'm' + Math.round(s%60) }}
+            {...props} />
 }
